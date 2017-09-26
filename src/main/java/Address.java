274462ae7c69fca4ed6db08494142e68db1a8d56
Tyler Jurecki <jurecki07@yahoo.com>
2017-09-26 11:31:02 -0500

    complete

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -1,16 +1,25 @@
+import org.apache.commons.lang3.StringUtils;
+
 public class Address {
     private String street;
 
     public String getStreet() {
         return street;
+
+
     }
 
     public void setStreet(String street) {
+        StringUtils.isBlank(street);      // Started to type this and you have to import from Apache
+
     }
+
 
     public static void main(String[] args) {
         Address address = new Address();
         address.setStreet(null);
     }
+
+
 }
 
--- src/test/java/Address.java
+++ src/test/java/Address.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import org.apache.commons.lang3.StringUtils;
 
 public class Address {
+    private String street;
+
+    public String getStreet() {
+        return street;
+
+
+    }
+
+    public void setStreet(String street) throws IllegalArgumentException {
+       if (StringUtils.isBlank(street)) {
+           throw new IllegalArgumentException();
+       }
+       }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        Address address = new Address();
+        address.setStreet(null);
+    }
+
+
 }
